b49f76a3aba4c6dfe2c6a2e5747725839116e45d
Desire Mukucha <muk6029@s286968.nml.com>
2018-07-05 16:02:10 -0500

    change gone come

--- code/background.js
+++ code/background.js
@@ -5,13 +5,13 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#000000'}, function() {
-    console.log("The color is black.");
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { urlContains: '' },
+        pageUrl: {hostEquals: 'developer.chrome.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
