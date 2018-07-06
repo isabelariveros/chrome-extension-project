93c4c7ae993dc8bd7dd8942c8522d5a035fa6747
Malcolm-Schultz <malcolmwschultz@gmail.com>
2018-07-06 10:21:20 -0500

    Set up server and client data transfer

--- code/apidata.js
+++ code/apidata.js
@@ -1,11 +1,7 @@
 function getSummId(summName){
     console.log("Here is the summoner name used: ", summName);
-    fetch("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summName + "?api_key=RGAPI-861be359-25cf-4cac-a289-732558d50429", {
-        headers: {'Access-Control-Allow-Origin': '*'},
-        method: 'GET'
+    fetch("http://localhost:3000/lolapi")
+    .then(results => {
+        console.log("hello", results.json());
     })
-        .then(results => {
-            console.log(results);
-        })
-
 }
